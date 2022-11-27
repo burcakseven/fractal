c726bd2cf79bbdddac88aa36028bc0d7006e47eb
burcakseven <burcakseven123@gmail.com>
2022-11-27 14:29:12 +0300

    decreased the window && tried trasparency

--- fractal.c
+++ fractal.c
@@ -8,17 +8,22 @@ void init_window(char* fractal_name)
     void	*mlx_win;
 
 	mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, fractal_name);
-	img.img = mlx_new_image(mlx, 1920, 1080);
+    mlx_win = mlx_new_window(mlx, 1920/2, 1080/2, fractal_name);
+	img.img = mlx_new_image(mlx, 1920/2, 1080/2);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian); 
-     int i = img.line_length;
+     int i = 100;
+	int colour;
+	
     while (i)
     {
-    int colour = create_trgb(i-1,i-5,i-7,i);
-	my_mlx_pixel_put(&img, i--, 5, colour);
+	colour = create_trgb(200,200,1,1);
+    
+	my_mlx_pixel_put(&img, 1920/4, 1080/4-i, colour);
+	i--;
        
     }
+	my_mlx_pixel_put(&img, 1920/4, 1080/4, colour);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 
